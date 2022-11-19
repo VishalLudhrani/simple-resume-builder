b9cf0abe7a7a4f371c154e564db3d7197c274704
Vishal Ludhrani <vludrani@gmail.com>
2022-11-19 08:54:38 +0530

    chore: app setup

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+// declare (file) extensions or anything that isn't built-in into typescript
+
+declare module '*.webp';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
